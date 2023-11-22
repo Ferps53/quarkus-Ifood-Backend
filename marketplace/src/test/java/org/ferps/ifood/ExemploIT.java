b76dd927a5dec3624b4c8a1dd3c3539452589dda
Felipe Brostolin <felipe.brostolin53@gmail.com>
2023-11-22 11:37:47 -0300

    Revert "first commit"
    This reverts commit bfafc9d2577f2c6de94123025e6ade097ac6f6ca.

--- marketplace/src/test/java/org/ferps/ifood/ExemploIT.java
+++
@@ -1,8 +0,0 @@
-package org.ferps.ifood;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ExemploIT extends ExemploTest {
-    // Execute the same tests but in packaged mode.
-}
